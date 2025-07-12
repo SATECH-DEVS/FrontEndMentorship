56c6acdd69950c124cb801f2331723df09615fa0
SATECH-DEVS.Saturday <SATECH-DEVS.saturdayemunefe77@gmail.com>
2025-07-12 16:41:13 +0100

    frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='bg-black text-white'>
+        WELCOME TO OUR FRONTEND
+        </div>
+    </>
+  )
+}
+
+export default App
